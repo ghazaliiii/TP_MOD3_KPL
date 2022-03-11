4219b9a87359cf0ccf9002b8464edc7c45cad13c
ghazaliiii <albanifaishal5@gmail.com>
2022-03-11 14:27:18 +0700

    menambahkan implementasi dengan state-based construction

---
+++ src/kpl_tp_mod3_se/pkg44/pkg03_1302200103/mainDoorMachine.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kpl_tp_mod3_se.pkg44.pkg03_1302200103;
+
+/**
+ *
+ * @author faishal
+ */
+import java.util.Scanner;
+public class mainDoorMachine {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        DoorMachine pintu = new DoorMachine();
+        System.out.println(pintu.getState());
+        System.out.print("Command : ");
+        String c = input.nextLine();
+        pintu.pintu(c);
+        System.out.println(pintu.getState());
+        System.out.print("Command : ");
+        c = input.nextLine();
+        pintu.pintu(c);
+        System.out.println(pintu.getState());
+        input.close();        
+    }
+    
+}
+
